4f1a4e810d6d984f90404a2af5d2a6edee6c719e
Rojas Ramirez <crojaram@med.umich.edu>
2023-12-20 10:53:20 -0500

    let's not ignore vignette files

---
+++ doc/Introduction.R
@@ -0,0 +1,43 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+
+## ----setup--------------------------------------------------------------------
+library(FragPipeToTPPR, TPP)
+
+## ----Path to local Frag Pipe folder-------------------------------------------
+FPpath <- FragPipeToTPPR::dummyFragPipepath
+
+## ----FragPipePath for building , include=FALSE--------------------------------
+FPpath<- "C:/Users/crojaram/Documents/FragPipeToTPPR/inst/extdata/dummyFragPipeFolder"
+
+## ----Experimental lables------------------------------------------------------
+explabels <- FragPipeToTPPR::dummyexplabels
+
+## ----TMT-Integrator (in FragPipe) ouput to TPP-R input------------------------
+configpath <- FragPipeToTPPR::tmtitotppr(FPpath, explabels)
+
+## ----ConfigPath for building , include=FALSE----------------------------------
+configpath <- "C:/Users/crojaram/Documents/FragPipeToTPPR/inst/extdata/dummyFragPipeFolder/1DTPP-TPPR/TPP-TR_config.csv"
+
+## ----Check path of previous output--------------------------------------------
+print(configpath)
+
+## ----Database for building , include=FALSE------------------------------------
+hoomandb <- "C:/Users/crojaram/Documents/FragPipeToTPPR/inst/extdata/dummyFragPipeFolder/2023-07-12-UP000005640_dummy.fas"
+
+## ----Melting Curve Normalization----------------------------------------------
+FragPipeToTPPR::tpprNormOneDTPP(configpath,FPpath)
+
+## ----TPP-R to TP-MAP----------------------------------------------------------
+
+FragPipeToTPPR::tpprTotpmap(configpath,hoomandb)
+
+## ----From TPP-R to TP-MAP-----------------------------------------------------
+TPPRfolder <- file.path(FPpath, "1DTPP-TPPR")
+TPPTR_result <- TPP::analyzeTPPTR(configpath,resultPath = TPPRfolder , methods = "meltcurvefit",idVar= "Prot_ID",qualColName="qssm")
+
+
