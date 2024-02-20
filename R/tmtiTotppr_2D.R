e3531dec53619336df658ae7eb976bf0e2341bd7
crojaram <crojaram@umich.edu>
2024-02-20 16:13:51 -0500

    Fixing bugs when creating TPPR compatible 2DTPP files

--- R/tmtiTotppr_2D.R
+++ R/tmtiTotppr_2D.R
@@ -6,8 +6,8 @@
 #' @return Path to the created TPP-R configuration file
 #' @export
 #'
-#' @examples tmtitpttpr("C:/FragPipeOutputfolder", c("42_44","46_48","50_52","54_56","58_60","62_64"), c(0,0.005,0.05,0.5,2))
-tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels){
+#' @examples tmtitpttpr("C:/FragPipeOutputfolder", c("42_44","46_48","50_52","54_56","58_60","62_64"), c(0,0.005,0.05,0.5,2), c(2mg ATP))
+tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels, compound){
 
   #Variables to find the correct TMTI .tsv file
   tmtifile <- "tmt-report/ratio_protein_None.tsv"
@@ -19,7 +19,7 @@ tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels)
   #Extract column names
   headers <- colnames(tmtidata)
 
-  #print(headers)
+  print(headers)
 
   # List all folders in the specified directory
   folders <- list.dirs(fragpipefolder, recursive = FALSE)
@@ -31,9 +31,9 @@ tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels)
 
   #Feb 16th: Replace first three with Compound, Experiment and Temperature
 
+  Compound <- c()
   Experiment <- c()
-  Condition <- c()
-  Replicate <- c()
+  Temperature <- c()
   TMTsix <- c()
   TMTsevenL <- c()
   TMTsevenH <- c()
@@ -51,7 +51,7 @@ tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels)
 
   #Find the experimental names
   for (folder in folders) {
-    foundexplabel <- ""
+
 
     # Select experiment folders to extract the number for replicates and
     #look for those labels among the columns of the tsv file
@@ -65,48 +65,36 @@ tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels)
 
         #print(path_components)
 
+        #Adding to the columns of the future config file
+        Compound <- append(compound, compound)
         temperatures_replabel <- path_components[length(path_components)]
-
         Experiment <- append(Experiment, temperatures_replabel)
-
-        #print(Experiment)
-
         conditionlabel <- unlist(strsplit(temperatures_replabel, "_"))
 
-        #Adding to the columns of the future config file
-        Condition <- append(Condition, conditionlabel[1])
-        Condition <- append(Condition, conditionlabel[2])
-        #Double up due to the precense of two temperatures
-        Replicate <- append(Replicate, conditionlabel[3])
-        Replicate <- append(Replicate, conditionlabel[3])
-
         #print(conditionlabel)
-
+        temp_conce_labels_ls <- list()
         for (item in conditionlabel[1:2]) {
-          print(item)
+          #Temperature
+          #print(item)
           for (conce_val in concentrationlabels){
+            #print(conce_val)
             temp_conc_label <- paste(item,conce_val, sep = "_")
-
-
-            #Slice data frame according to the experiment and replicate
-            for (item in headers) {
-              if (grepl(temp_conc_label, item)){
-                #print(item)
-                # Remove the first 'X' character from the string, which was added when using read.delim()
-                lscolumnstoget[[length(lscolumnstoget) + 1]] <- item
-
-              }
+            lscolumnstoget[[length(lscolumnstoget) + 1]] <- paste("X",temp_conc_label, sep="")
+            #print(temp_conc_label)
             }
-
-          }}
-
-        print(lscolumnstoget)
+          }
 
 
+
+
+        #print(paste("lscolumnstoget",lscolumnstoget))
         # Selecting columns by name
         newdataframe <- tmtidata[, unlist(lscolumnstoget)]
-        print(class(newdataframe))
+        #print(class(newdataframe))
         #print(newdataframe$"Index")
+
+       # print(colnames(newdataframe))
+
         #Normalization to lowest temperature (first two columns are protein id and qssm filtering criteria)
         newdataframe[, 3:ncol(newdataframe)] <-   newdataframe[, 3:ncol(newdataframe)] / newdataframe[,3]
 
@@ -114,7 +102,31 @@ tmtitotppr_2D <- function(fragpipefolder, experimentlabels, concentrationlabels)
 
         #TODO: Use annotation file to replace the temperature_experiment with appropriate TMT -labels
         tpprheaders <- tmtiheader_to_tpprheaders(folder)
-        print(tpprheaders)
+        #print(tpprheaders)
+
+        #Extract the TMT-label to Celcius conversion
+        configtempvals <- tpprheaders[2][[1]]
+
+
+        TMTsix <- append(TMTsix, configtempvals$`126` )
+        TMTsevenL <- append(TMTsevenL, configtempvals$`127L` )
+        TMTsevenH <- append(TMTsevenH, configtempvals$`127H` )
+        TMTeightL <- append(TMTeightL, configtempvals$`128L` )
+        TMTeightH <- append(TMTeightH, configtempvals$`128H` )
+        TMTnineL <- append(TMTnineL, configtempvals$`129L` )
+        TMTnineH <- append(TMTnineH, configtempvals$`129H` )
+        TMTtenL <- append(TMTtenL, configtempvals$`130L` )
+        TMTtenH <- append(TMTtenH, configtempvals$`130H` )
+        TMTelevenL <- append(TMTelevenL, configtempvals$`131L` )
+
+        #(colnames(newdataframe))
+        #print(tpprheaders[1])
+        #Place correct headers
+        names(newdataframe) <- unlist(tpprheaders[1])
+        print(newdataframe)
+
+        outputfilename <- paste(temperatures_replabel, "txt", sep = ".")
+
 
       }}
   }
